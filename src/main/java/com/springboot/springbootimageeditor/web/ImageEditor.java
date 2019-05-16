48b024214dca0a801739b4acc155beb19d2650b7
MaciejRumas <maciej.rumas@gmail.com>
2019-05-16 15:39:18 +0200

    Added Gaussian blur

--- src/main/java/com/springboot/springbootimageeditor/web/ImageEditor.java
+++ src/main/java/com/springboot/springbootimageeditor/web/ImageEditor.java
@@ -1,5 +1,6 @@
 package com.springboot.springbootimageeditor.web;
 
+import org.apache.tomcat.websocket.ReadBufferOverflowException;
 import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -7,25 +8,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.awt.Color;
-import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.websocket.server.PathParam;
 
 @RestController
 public class ImageEditor {
 
     private ImageProcessorController imageProcessorController = new ImageProcessorController();
-
-    @RequestMapping("/hello")
-    public String sayHello(@RequestParam(value = "name") String name) {
-        return "Hello " + name + "!";
-    }
 
     @RequestMapping(value = "/image", method = RequestMethod.POST)
     public JSONObject addImage(HttpServletRequest requestEntity) throws Exception {
@@ -33,70 +26,76 @@ public class ImageEditor {
     }
 
     @RequestMapping(value = "/image/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity deleteImage(@PathVariable String id){
-        if(imageProcessorController.imageMapContains(id)){
+    public ResponseEntity deleteImage(@PathVariable String id) {
+        if (imageProcessorController.imageMapContains(id)) {
             imageProcessorController.deleteImage(id);
             return null;
-        }else{
+        } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ID not found");
         }
     }
 
     @RequestMapping(value = "/image/{id}/size", method = RequestMethod.GET)
-    public JSONObject getSize(@PathVariable String id){
-        if(imageProcessorController.imageMapContains(id)){
+    public JSONObject getSize(@PathVariable String id) {
+        if (imageProcessorController.imageMapContains(id)) {
             return imageProcessorController.getSize(id);
-        }else{
+        } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ID not found");
         }
     }
 
     @RequestMapping(value = "/image/{id}/scale/{percent}", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
-    public byte[] getRescaledImage(@PathVariable String id, @PathVariable double percent)  throws Exception {
-        if(imageProcessorController.imageMapContains(id)){
+    public byte[] getRescaledImage(@PathVariable String id, @PathVariable double percent) throws IOException {
+        if (imageProcessorController.imageMapContains(id)) {
             return imageProcessorController.getScaledImage(id, percent);
-        }else{
+        } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ID not found");
         }
     }
 
     @RequestMapping(value = "/image/{id}/histogram", method = RequestMethod.GET)
     public JSONObject getHistogram(@PathVariable String id) {
-        if(imageProcessorController.imageMapContains(id)){
+        if (imageProcessorController.imageMapContains(id)) {
             return imageProcessorController.getHistogram(id);
-        }else{
+        } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ID not found");
         }
     }
 
 
     @RequestMapping(value = "/image/{id}/crop/{start}/{stop}/{width}/{height}", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
-    public byte[] getRescaledImage(@PathVariable String id, @PathVariable int start, @PathVariable int stop, @PathVariable int width, @PathVariable int height)  throws Exception {
-        if(imageProcessorController.imageMapContains(id)){
+    public byte[] getRescaledImage(@PathVariable String id, @PathVariable int start, @PathVariable int stop, @PathVariable int width, @PathVariable int height) throws IOException {
+        if (imageProcessorController.imageMapContains(id)) {
             try {
                 return imageProcessorController.getCroppedImage(id, start, stop, width, height);
-            }catch(ResponseStatusException e){
+            } catch (ResponseStatusException e) {
                 throw new ResponseStatusException(e.getStatus(), e.getMessage());
             }
-        }
-        else{
+        } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found");
         }
     }
 
 
     @RequestMapping(value = "/image/{id}/greyscale", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
-    public byte[] getRescaledImage(@PathVariable String id)  throws Exception {
-        if(imageProcessorController.imageMapContains(id)){
+    public byte[] getRescaledImage(@PathVariable String id) throws IOException {
+        if (imageProcessorController.imageMapContains(id)) {
             return imageProcessorController.getGreyScale(id);
-        }
-        else{
+        } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Image not found");
         }
     }
 
+    @RequestMapping(value = "/image/{id}/blur/{radius}", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getBlurredImage(@PathVariable String id, @PathVariable float radius) throws IOException {
+        if (imageProcessorController.imageMapContains(id)) {
+            if(radius>0) {
+                return imageProcessorController.getBlurredImage(id, radius);
+            }
+            else throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Radius must be  > 0");
+        } else throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Image not found");
 
-
+    }
 
 
 }
--- src/main/java/com/springboot/springbootimageeditor/web/ImageProcessorController.java
+++ src/main/java/com/springboot/springbootimageeditor/web/ImageProcessorController.java
@@ -31,7 +31,7 @@ public class ImageProcessorController {
         BufferedImage image = ImageIO.read(inputStream);
 
         String uniqueID = UUID.randomUUID().toString();
-        imageMap.put(uniqueID,image);
+        imageMap.put(uniqueID, image);
 
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("ID", uniqueID);
@@ -42,14 +42,13 @@ public class ImageProcessorController {
     }
 
     public void deleteImage(String id) {
-            imageMap.remove(id);
+        imageMap.remove(id);
     }
 
-    public boolean imageMapContains(String id){
-        if(imageMap.containsKey(id)) {
+    public boolean imageMapContains(String id) {
+        if (imageMap.containsKey(id)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
@@ -69,46 +68,46 @@ public class ImageProcessorController {
         return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
     }
 
-    private BufferedImage bicubicScaling(BufferedImage image, double percentage){
+    private BufferedImage bicubicScaling(BufferedImage image, double percentage) {
         BufferedImage clone = deepCopy(image);
-        BufferedImage scaled = new BufferedImage((int)(image.getWidth()*percentage/100), (int)(image.getHeight()*percentage/100), BufferedImage.TYPE_INT_RGB);
+        BufferedImage scaled = new BufferedImage((int) (image.getWidth() * percentage / 100), (int) (image.getHeight() * percentage / 100), BufferedImage.TYPE_INT_RGB);
 
         Graphics2D graphics2D = scaled.createGraphics();
         graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
                 RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-        graphics2D.scale(percentage/100, percentage/100);
+        graphics2D.scale(percentage / 100, percentage / 100);
         graphics2D.drawImage(clone, 0, 0, null);
         graphics2D.dispose();
 
         return scaled;
     }
 
-    public byte[] getScaledImage(String id, double percentage) throws IOException{
-        BufferedImage scaledImage = bicubicScaling(imageMap.get(id),percentage);
+    public byte[] getScaledImage(String id, double percentage) throws IOException {
+        BufferedImage scaledImage = bicubicScaling(imageMap.get(id), percentage);
         return bufferedImageToByteArray(scaledImage);
     }
 
     private byte[] bufferedImageToByteArray(BufferedImage image) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write( image, "png", baos );
+        ImageIO.write(image, "png", baos);
         baos.flush();
         byte[] imageInByte = baos.toByteArray();
         baos.close();
         return imageInByte;
     }
 
-    public JSONObject getHistogram(String id){
+    public JSONObject getHistogram(String id) {
         BufferedImage image = imageMap.get(id);
         double R[] = new double[256];
         double G[] = new double[256];
         double B[] = new double[256];
         int rgb, r, g, b;
 
-        for(int i=0;i<image.getWidth();i++){
-            for (int j = 0;j < image.getHeight();j++){
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
                 rgb = image.getRGB(i, j);
-                r = (rgb>>16) & 0xff;
-                g = (rgb>>8) & 0xff;
+                r = (rgb >> 16) & 0xff;
+                g = (rgb >> 8) & 0xff;
                 b = (rgb) & 0xff;
                 R[r]++;
                 G[g]++;
@@ -116,68 +115,88 @@ public class ImageProcessorController {
             }
         }
 
+        double maxR = R[0];
+        double maxG = G[0];
+        double maxB = B[0];
+
+        for(int i=1;i<255;i++){
+            if(maxR < R[i])
+                maxR = R[i];
+            if(maxG < G[i])
+                maxG = G[i];
+            if(maxB < B[i])
+                maxB = B[i];
+        }
+
+
         JSONObject jsonObject = new JSONObject();
         JSONObject rObject = new JSONObject();
         JSONObject gObject = new JSONObject();
         JSONObject bObject = new JSONObject();
 
-        for(int i=0;i<256;i++) {
-            //R[i] /= 255;
-            //G[i] /= 255;
-            //B[i] /= 255;
+        for (int i = 0; i < 256; i++) {
+            R[i] /= maxR;
+            G[i] /= maxR;
+            B[i] /= maxR;
 
-            rObject.put(i,R[i]);
-            gObject.put(i,G[i]);
-            bObject.put(i,B[i]);
+            rObject.put(i, R[i]);
+            gObject.put(i, G[i]);
+            bObject.put(i, B[i]);
         }
-        jsonObject.put("R",rObject);
-        jsonObject.put("G",gObject);
-        jsonObject.put("B",bObject);
+        jsonObject.put("R", rObject);
+        jsonObject.put("G", gObject);
+        jsonObject.put("B", bObject);
 
         return jsonObject;
     }
 
-    public byte[] getCroppedImage(String id, int start, int stop, int width, int height) throws IOException, ResponseStatusException{
-        if((start >= 0 && start <= imageMap.get(id).getWidth()) && stop >= 0 && stop <= imageMap.get(id).getHeight() && (imageMap.get(id).getWidth() - start) >= width && (imageMap.get(id).getHeight() - stop >= height) ){
+    public byte[] getCroppedImage(String id, int start, int stop, int width, int height) throws IOException, ResponseStatusException {
+        if ((start >= 0 && start <= imageMap.get(id).getWidth()) && stop >= 0 && stop <= imageMap.get(id).getHeight() && (imageMap.get(id).getWidth() - start) >= width && (imageMap.get(id).getHeight() - stop >= height)) {
 
             BufferedImage croppedImage = imageMap.get(id).getSubimage(start, stop, width, height);
 
-           return bufferedImageToByteArray(croppedImage);
-        }
-        else{
+            return bufferedImageToByteArray(croppedImage);
+        } else {
             throw new ResponseStatusException(
                     HttpStatus.NOT_ACCEPTABLE, "One of the arguments is not correct!"
             );
         }
     }
 
-    public byte[] getGreyScale(String id)throws Exception
-    {
-            BufferedImage clone = deepCopy(imageMap.get(id));
+    public byte[] getGreyScale(String id) throws IOException {
+        BufferedImage clone = deepCopy(imageMap.get(id));
 
-            int rgb, r, g, b, R, G, B;
-            for(int i =0;i<clone.getWidth();i++)
-            {
-                for(int j =0;j<clone.getHeight();j++)
-                {
-                    rgb = clone.getRGB(i, j);
-                    r = (rgb>>16) & 0xff;
-                    g = (rgb>>8) & 0xff;
-                    b = (rgb) & 0xff;
-                    R = (int) (r * 0.29);
-                    G = (int) (g * 0.587);
-                    B = (int) (b * 0.114);
-                    Color grey = new Color(R + G + B, R + G + B, R + G + B);
-                    clone.setRGB(i, j, grey.getRGB());
-                }
+        int rgb, r, g, b, R, G, B;
+        for (int i = 0; i < clone.getWidth(); i++) {
+            for (int j = 0; j < clone.getHeight(); j++) {
+                rgb = clone.getRGB(i, j);
+                r = (rgb >> 16) & 0xff;
+                g = (rgb >> 8) & 0xff;
+                b = (rgb) & 0xff;
+                R = (int) (r * 0.29);
+                G = (int) (g * 0.587);
+                B = (int) (b * 0.114);
+                Color grey = new Color(R + G + B, R + G + B, R + G + B);
+                clone.setRGB(i, j, grey.getRGB());
             }
-            BufferedImage image = new BufferedImage(clone.getWidth(), clone.getHeight(), BufferedImage.TYPE_INT_RGB);
-            Graphics2D bg = image.createGraphics();
+        }
+        BufferedImage image = new BufferedImage(clone.getWidth(), clone.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics2D bg = image.createGraphics();
 
-            bg.drawImage(clone, 0, 0, null);
-            bg.dispose();
+        bg.drawImage(clone, 0, 0, null);
+        bg.dispose();
 
-            return bufferedImageToByteArray(image);
+        return bufferedImageToByteArray(image);
+    }
+
+    public byte[] getBlurredImage(String id, float radius) throws IOException {
+        BufferedImage image = imageMap.get(id);
+        BufferedImage clone = deepCopy(image);
+
+        GaussianFilter gaussianFilter = new GaussianFilter(radius, 100);
+        clone = gaussianFilter.filter(image, clone, true);
+
+        return bufferedImageToByteArray(clone);
     }
 
 }
